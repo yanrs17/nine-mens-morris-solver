42ff6cbda6e852ac97633744026aa005f0eb8c2f
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-08-07 20:12:50 -0400

    Add test case

---
+++ test_unit.py
@@ -0,0 +1,26 @@
+from game import Game
+from state import State
+from strategy_random import StrategyRandom
+
+def test1():
+    """
+    Init from Phase 1.
+    """
+    Game(State, StrategyRandom, grid = []).play()
+
+def test2():
+    """
+    Init from Phase 2, convenient for testing.
+    """
+    grid = [
+        [2,  -1, -1,  2, -1, -1,  2],
+        [-1,  2, -1,  2, -1,  2, -1],
+        [-1, -1,  2,  2,  2, -1, -1],
+        [1,   1,  1, -1,  1,  1,  1],
+        [-1, -1,  1,  1,  1, -1, -1],
+        [-1,  0, -1,  0, -1,  0, -1],
+        [0,  -1, -1,  0, -1, -1,  0]
+    ]
+    Game(State, StrategyRandom, grid = grid).play()
+
+test2()
